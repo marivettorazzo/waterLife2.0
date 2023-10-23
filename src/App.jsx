68a361f2c22b66e6758720a8a0f315e10eeca569
Mariana Vettorazzo <marianavettorazzo@gmail.com>
2023-10-23 12:54:37 -0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.scss'
+import HeaderUser from './components/header/HeaderUser.jsx'
+
+function App() {
+
+
+  return (
+    <>
+     <HeaderUser/>
+     <p>body aqui</p>
+    </>
+  )
+}
+
+export default App
+// Mariana Vettorazzo
---
+++ src/components/header/HeaderUser.jsx
@@ -0,0 +1,21 @@
+const HeaderUser = () => {
+  return (
+    <div>
+      <header>
+        <div>
+          <ol>
+            <li>
+              <img src="" alt="" />
+            </li>
+            <li>Promoções</li>
+            <li>Sobre Nós</li>
+            <li>Carrinho</li>
+          </ol>
+        </div>
+        
+      </header>
+    </div>
+  );
+};
+
+export default HeaderUser;
