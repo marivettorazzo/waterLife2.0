d66f43645b7372f3028d68050233f1aaa8bd3455
Mariana Vettorazzo <marianavettorazzo@gmail.com>
2023-10-24 19:46:40 -0300

    feat/ adjustment in styles cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -12,18 +12,44 @@ const pathsImagesCards = [
 ];
 export default function Cards() {
   let [userMaster] = useState(true);
+  const create = (e) => {
+    console.log(e, "criei um card");
+  };
+  const atualizar = (e) => {
+    console.log(e, "atualizei um card");
+  };
+  const deletar = (e) => {
+    console.log(e, "deletei um card");
+  };
 
   return (
-    <div className="contentCardcard">
-      {userMaster && <button className="create buttonsHome">Criar Card</button>}
+    <div>
+      <div className="contentCreatButton">
+        {userMaster && (
+          <button className="create buttonsHome " onClick={create}>
+            Criar Card
+          </button>
+        )}
+      </div>
       <div className="contentCards">
         <div className="cards">
           {pathsImagesCards.map((item, i) => {
             return (
               <div key={i} className="internoCards">
                 <img className="imageCard" src={item.img} />
-                {userMaster && <button className="alert buttonsHome">Editar</button>}
-                {userMaster && <button className="deleteOrCancel buttonsHome ">Deletar</button>}
+                {userMaster && (
+                  <button onClick={atualizar} className="alert buttonsHome">
+                    Editar
+                  </button>
+                )}
+                {userMaster && (
+                  <button
+                    onClick={deletar}
+                    className="deleteOrCancel buttonsHome "
+                  >
+                    Deletar
+                  </button>
+                )}
               </div>
             );
           })}
