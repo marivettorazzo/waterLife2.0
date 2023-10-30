7e16dd41cb7db062a81c14f718a0809a1c214cb5
Mariana Vettorazzo <marianavettorazzo@gmail.com>
2023-10-30 17:25:18 -0300

    feat/adjustment in footer position

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,7 +9,7 @@ import './header.scss';
       <div>
         <div className="nav">
           <div className="logo">
-            <img src='/images/logoWater.png' alt='logo'/>
+            <img className='img-fluid' src='/images/logoWater.png' alt='logo'/>
           </div>
           <div className="nav-item">Promoções</div>
           <div className="nav-item">Sobre Nós</div>
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,38 +1,75 @@
-import "./sobre.scss"
+import "./sobre.scss";
 const Sobre = () => {
   return (
     <div className="contentSobreWater">
       <h1>NOSSA HISTÓRIA!</h1>
       <div className="contentCardHistory">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, dolores repudiandae accusantium, tenetur iure delectus distinctio molestias blanditiis unde beatae quia corporis odit vero reiciendis numquam ullam, reprehenderit voluptas quos!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, laborum! Optio labore omnis architecto assumenda debitis odit voluptatem exercitationem. Saepe debitis necessitatibus nobis nemo quae laudantium nisi sunt assumenda dicta!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque esse cum a, soluta tempora id facere tenetur, placeat accusamus reprehenderit illum error, quae velit molestias molestiae laboriosam deleniti veniam eius!
-          </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+          dolores repudiandae accusantium, tenetur iure delectus distinctio
+          molestias blanditiis unde beatae quia corporis odit vero reiciendis
+          numquam ullam, reprehenderit voluptas quos! Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Eveniet, laborum! Optio labore omnis
+          architecto assumenda debitis odit voluptatem exercitationem. Saepe
+          debitis necessitatibus nobis nemo quae laudantium nisi sunt assumenda
+          dicta! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque
+          esse cum a, soluta tempora id facere tenetur, placeat accusamus
+          reprehenderit illum error, quae velit molestias molestiae laboriosam
+          deleniti veniam eius!
+        </p>
         <div className="childOne">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, dolores repudiandae accusantium, tenetur iure delectus distinctio molestias blanditiis unde beatae quia corporis odit vero reiciendis numquam ullam, reprehenderit voluptas quos!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, laborum! Optio labore omnis architecto assumenda debitis odit voluptatem exercitationem. Saepe debitis necessitatibus nobis nemo quae laudantium nisi sunt assumenda dicta!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque esse cum a, soluta tempora id facere tenetur, placeat accusamus reprehenderit illum error, quae velit molestias molestiae laboriosam deleniti veniam eius!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            dolores repudiandae accusantium, tenetur iure delectus distinctio
+            molestias blanditiis unde beatae quia corporis odit vero reiciendis
+            numquam ullam, reprehenderit voluptas quos! Lorem ipsum dolor sit
+            amet consectetur, adipisicing elit. Eveniet, laborum! Optio labore
+            omnis architecto assumenda debitis odit voluptatem exercitationem.
+            Saepe debitis necessitatibus nobis nemo quae laudantium nisi sunt
+            assumenda dicta! Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Atque esse cum a, soluta tempora id facere tenetur, placeat
+            accusamus reprehenderit illum error, quae velit molestias molestiae
+            laboriosam deleniti veniam eius!
           </p>
-          <img  className="img-fluid" src="../../public/images/amarelo.jpg" alt="Espécie de peixe" />
+          <img
+            className="img-fluid"
+            src="../../public/images/amarelo.jpg"
+            alt="Espécie de peixe"
+          />
         </div>
         <div className="childTwo">
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, laborum! Optio labore omnis architecto assumenda debitis odit voluptatem exercitationem. Saepe debitis necessitatibus nobis nemo quae laudantium nisi sunt assumenda dicta!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque esse cum a, soluta tempora id facere tenetur, placeat accusamus reprehenderit illum error, quae velit molestias molestiae laboriosam deleniti veniam eius!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis porro nemo quo vel. Nemo quia laborum deleniti, quaerat reprehenderit atque nesciunt. Amet ipsam architecto molestiae quod, repudiandae iure quisquam rem.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet,
+            laborum! Optio labore omnis architecto assumenda debitis odit
+            voluptatem exercitationem. Saepe debitis necessitatibus nobis nemo
+            quae laudantium nisi sunt assumenda dicta! Lorem ipsum dolor sit
+            amet consectetur, adipisicing elit. Atque esse cum a, soluta tempora
+            id facere tenetur, placeat accusamus reprehenderit illum error, quae
+            velit molestias molestiae laboriosam deleniti veniam eius! Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit. Officiis porro
+            nemo quo vel. Nemo quia laborum deleniti, quaerat reprehenderit
+            atque nesciunt. Amet ipsam architecto molestiae quod, repudiandae
+            iure quisquam rem.
           </p>
-          <img className="img-fluid"  src="../../public/images/laranjinha.jpg" alt="Espécie de peixe" />
+          <img
+            className="img-fluid"
+            src="../../public/images/laranjinha.jpg"
+            alt="Espécie de peixe"
+          />
         </div>
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, dolores repudiandae accusantium, tenetur iure delectus distinctio molestias blanditiis unde beatae quia corporis odit vero reiciendis numquam ullam, reprehenderit voluptas quos!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, laborum! Optio labore omnis architecto assumenda debitis odit voluptatem exercitationem. Saepe debitis necessitatibus nobis nemo quae laudantium nisi sunt assumenda dicta!
-            
-          </p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+          dolores repudiandae accusantium, tenetur iure delectus distinctio
+          molestias blanditiis unde beatae quia corporis odit vero reiciendis
+          numquam ullam, reprehenderit voluptas quos! Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Eveniet, laborum! Optio labore omnis
+          architecto assumenda debitis odit voluptatem exercitationem. Saepe
+          debitis necessitatibus nobis nemo quae laudantium nisi sunt assumenda
+          dicta!
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sobre
+export default Sobre;
