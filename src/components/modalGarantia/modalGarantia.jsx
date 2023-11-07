b561644b7c8a4154b98aff9a9856cc728742b36d
Mariana Vettorazzo <marianavettorazzo@gmail.com>
2023-11-07 14:29:53 -0300

    feat/adjustment in modalGarantia

---
+++ src/components/modalGarantia/modalGarantia.jsx
@@ -0,0 +1,18 @@
+
+import "../botoesSales/buttonsSales.scss";
+
+const ModalGarantia = () => {
+   
+    return (
+        <div>
+            <button type="button" className="btn btn-primary" >
+                Launch demo modal
+            </button>
+
+
+            
+        </div>
+    );
+};
+
+export default ModalGarantia;
